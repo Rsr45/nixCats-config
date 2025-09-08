5a999d7b809847b72dbb0ff06ebe9f64b342c3c4
Rsr45 <efe45676@gmail.com>
2025-09-08 23:54:58 +0300

    fzf-lua

--- lua/myLuaConf/plugins/fzf.lua
+++ lua/myLuaConf/plugins/fzf.lua
@@ -7,7 +7,7 @@ return {
             { "<leader>sf",        "<cmd>FzfLua files<CR>",    mode = { "n" }, desc = '[S]earch [F]iles', },
             { "<leader><leader>s", "<cmd>FzfLua buffers<CR>",  mode = { "n" }, desc = '[ ] Find existing buffers', },
             { "<leader>s.",        "<cmd>FzfLua oldfiles<CR>", mode = { "n" }, desc = '[S]earch Recent Files ("." for repeat)', },
-            -- { "<leader>sr",        "<cmd>FzfLuafunction() return require('telescope.builtin').resume() end",      mode = { "n" }, desc = '[S]earch [R]esume', },
+            { "<leader>sr",        "<cmd>FzfLua resume<CR>",   mode = { "n" }, desc = '[S]earch [R]esume', },
             -- { "<leader>sd",        function() return require('telescope.builtin').diagnostics() end, mode = { "n" }, desc = '[S]earch [D]iagnostics', },
             -- { "<leader>sg",        function() return require('telescope.builtin').live_grep() end,   mode = { "n" }, desc = '[S]earch by [G]rep', },
             -- { "<leader>sw",        function() return require('telescope.builtin').grep_string() end, mode = { "n" }, desc = '[S]earch current [W]ord', },
