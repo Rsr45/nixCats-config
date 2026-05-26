0e62e07a754a1c82f77626911f3d602e4c417dbf
Rsr45 <efe54676@gmail.com>
2026-05-27 00:56:33 +0300

    xcddcd

--- lua/myLuaConf/opts_and_keys.lua
+++ lua/myLuaConf/opts_and_keys.lua
@@ -76,21 +76,21 @@ vim.o.conceallevel = 1
 -- [[ Disable auto comment on enter ]]
 -- See :help formatoptions
 vim.api.nvim_create_autocmd("FileType", {
-    desc = "remove formatoptions",
-    callback = function()
-        vim.opt.formatoptions:remove({ "c", "r", "o" })
-    end,
+	desc = "remove formatoptions",
+	callback = function()
+		vim.opt.formatoptions:remove({ "c", "r", "o" })
+	end,
 })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = highlight_group,
-    pattern = '*',
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = '*',
 })
 
 vim.g.netrw_liststyle = 0
@@ -105,6 +105,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Scroll Down' })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Scroll Up' })
 vim.keymap.set("n", "n", "nzzzv", { desc = 'Next Search Result' })
 vim.keymap.set("n", "N", "Nzzzv", { desc = 'Previous Search Result' })
+vim.keymap.set("n", "<S-Down>", "mzJ`z", { desc = 'Keep cursor at line start' })
 
 -- vim.keymap.set("n", "<leader><leader>[", "<cmd>bprev<CR>", { desc = 'Previous buffer' })
 -- vim.keymap.set("n", "<leader><leader>]", "<cmd>bnext<CR>", { desc = 'Next buffer' })
@@ -127,31 +128,29 @@ vim.keymap.set('n', 'down', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent =
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 -- vim.keymap.set('n', '<leader>cx', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 vim.keymap.set('n', '<leader>cx', function()
-    if vim.fn.getloclist(0, { winid = 1 }).winid ~= 0 then
-        vim.cmd('lclose')
-    else
-        vim.diagnostic.setloclist()
-    end
+	if vim.fn.getloclist(0, { winid = 1 }).winid ~= 0 then
+		vim.cmd('lclose')
+	else
+		vim.diagnostic.setloclist()
+	end
 end, { desc = 'Open diagnostics list' })
 
 -- You should instead use these keybindings so that they are still easy to use, but dont conflict
 vim.keymap.set({ "v", "x", "n" }, '<leader>y', '"+y', { noremap = true, silent = true, desc = 'Yank to clipboard' })
 vim.keymap.set({ "n", "v", "x" }, '<leader>Y', '"+yy', { noremap = true, silent = true, desc = 'Yank line to clipboard' })
 vim.keymap.set({ "n", "v", "x" }, '<C-a>', 'gg0vG$', { noremap = true, silent = true, desc = 'Select all' })
--- vim.keymap.set({ 'n', 'v', 'x' }, '<leader>p', '"+p', { noremap = true, silent = true, desc = 'Paste from clipboard' })
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>p', '"+p', { noremap = true, silent = true, desc = 'Paste from clipboard' })
 vim.keymap.set('i', '<C-p>', '<C-r><C-p>+',
-    { noremap = true, silent = true, desc = 'Paste from clipboard from within insert mode' })
+	{ noremap = true, silent = true, desc = 'Paste from clipboard from within insert mode' })
 vim.keymap.set("x", "<leader>P", '"_dP',
-    { noremap = true, silent = true, desc = 'Paste over selection without erasing unnamed register' })
+	{ noremap = true, silent = true, desc = 'Paste over selection without erasing unnamed register' })
 
 -- File manipulation
-vim.keymap.set("n", '<leader>fs', '<cmd>update<CR>', { desc = 'File save' })
--- spellcheck toggle
+-- vim.keymap.set("n", '<leader>fs', '<cmd>update<CR>', { desc = 'File save' })
 vim.keymap.set("n", '<leader>ts', '<cmd>set spell!<Cr>', { desc = 'Toggle Spellcheck' })
--- relativenumber toggle
 vim.keymap.set("n", '<leader>tl', '<cmd>set rnu!<CR>')
 --
-vim.keymap.set("n", '<leader>bN', '<cmd>ene | startinsert<CR>', { desc = 'New Empty Buffer' })
+-- vim.keymap.set("n", '<leader>bN', '<cmd>ene | startinsert<CR>', { desc = 'New Empty Buffer' })
 
 -- harpoon but builtin
 -- vim.keymap.set('n', "<leader><cr>", ":argu<cr>:args<cr>", { desc = "go to last used arglist file" })
@@ -163,3 +162,4 @@ vim.keymap.set("n", '<leader>bN', '<cmd>ene | startinsert<CR>', { desc = 'New Em
 -- vim.keymap.set('n', "<leader>aa", "<cmd>$argadd %<bar>argded<bar>args<cr>", { desc = "add to arglist" })
 -- vim.keymap.set('n', "<leader>ad", "<cmd>argdelete %<bar>args<cr>", { desc = "delete from arglist" })
 -- vim.keymap.set('n', "<leader>ac", "<cmd>argdelete *<CR><C-L>", { desc = "clear arglist" })
+--
